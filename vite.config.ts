25140e5f13c2c26a9146a02a8a2e67d0782a372d
BABB2025 <tony@thetriumphantgroup.com>
2025-11-04 19:19:45 -0800

    `Add figma asset plugin`

--- vite.config.ts
+++ vite.config.ts
@@ -10,17 +10,12 @@ function figmaAssetPlugin(): Plugin {
     name: 'figma-asset-plugin',
     resolveId(id) {
       if (id.startsWith('figma:asset/')) {
-        // Extract the hash from figma:asset/HASH.png
         const hash = id.replace('figma:asset/', '')
-        // Return a special ID that we'll handle in load
         return '\0figma-asset:' + hash
       }
     },
     load(id) {
       if (id.startsWith('\0figma-asset:')) {
-        const hash = id.replace('\0figma-asset:', '')
-        // Return a module that exports a placeholder or data URL
-        // For now, use a 1x1 transparent PNG as placeholder
         const placeholder = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
         return `export default "${placeholder}"`
       }
@@ -28,7 +23,6 @@ function figmaAssetPlugin(): Plugin {
   }
 }
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), figmaAssetPlugin()],
   build: {
@@ -51,3 +45,4 @@ export default defineConfig({
     port: 8080
   }
 })
+```
